5e8a8fe878eaadfa9966e082593065d25d867da5
Isaac Maamouche <maamouche.isaac@gmail.com>
2022-02-26 18:06:38 +0100

    contrast fix

---
+++ _next/static/YR7CYgowSf8xcw5cTJFlN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/241-32c61efa144c1aa0.js","static/chunks/pages/index-f38785ad76aa89e8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
