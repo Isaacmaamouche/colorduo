4fe346e0cf35a52855dd6873f63b0f8b8e9d1afa
Isaac Maamouche <maamouche.isaac@gmail.com>
2022-02-27 18:24:45 +0100

    name fix

---
+++ _next/static/F5Lf7wiK9lOGcqmqIFnMO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/241-32c61efa144c1aa0.js","static/chunks/pages/index-2376c1df6563ee09.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
